06193741ac6f26dda68cab110f225cf0a8d16e26
Cedric <young.cet@gmail.com>
2025-01-13 17:56:12 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() => runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payfast/payfast.dart';
+
+import 'hex-color.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PayFast Widget',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -21,7 +32,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'PayFast Widget'),
     );
   }
 }
@@ -45,68 +56,133 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+   static const platform = MethodChannel('com.xdev.payfast/payment');
+   Widget? _widget;
+   var jData;
+   bool showAppBar = true;
+   String appBarTitle = '';
+   String appBarBackgroundColor = '';
+   Color spinnerColor = Colors.red;
+   
+  @override
+  void initState() {
+    platform.setMethodCallHandler(_receiveFromHost);
+    super.initState();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void onPaymentCancelled() async {
+    try {
+      await platform.invokeMethod<void>('onPaymentCancelled');
+    } on PlatformException catch (e) {
+      debugPrint('Error during onPaymentCancelled: ${e.message}');
+    }
+  }
+
+  void onPaymentCompleted() async{
+    try {
+      await platform.invokeMethod<void>('onPaymentCompleted');
+    } on PlatformException catch (e) {
+      debugPrint('Error during onPaymentCompleted: ${e.message}');
+    }
+  }
+
+  Future<void> _receiveFromHost(MethodCall call) async {
+    bool useSandBox = true;
+    String passPhrase = '';
+    dynamic paymentData;
+    String onsiteActivationScriptUrl = '';
+    String? payButtonText;
+    String? onPaymentCancelledText;
+    String? onPaymentCompletedText;
+    String? paymentSummaryTitle;
+    Color? paymentSummaryAmountColor;
+
+    try {
+      if (call.method == "initialise") {
+        final String data = call.arguments;
+        jData = await jsonDecode(data);
+
+        useSandBox = jData['useSandBox'];
+        passPhrase = jData['passPhrase'];
+        paymentData = jData['data'];
+        onsiteActivationScriptUrl = jData['onsiteActivationScriptUrl'];
+
+        // optional parameters
+        if (jData.containsKey('appBar')){
+          showAppBar = jData['appBar']['show'];
+          appBarTitle = jData['appBar']['title'];
+          appBarBackgroundColor = jData['appBar']['backgroundColor'];
+        }
+        
+        if (jData.containsKey('spinnerColor')){
+          spinnerColor = HexColor(jData['spinnerColor']);
+        }
+
+        if (jData.containsKey('payButtonText')){
+          payButtonText = jData['payButtonText'];
+        }
+
+        if (jData.containsKey('onPaymentCancelledText')){
+          onPaymentCancelledText = jData['onPaymentCancelledText'];
+        }
+
+        if (jData.containsKey('onPaymentCompletedText')){
+          onPaymentCompletedText = jData['onPaymentCompletedText'];
+        }
+
+        if (jData.containsKey('paymentSummaryTitle')){
+          paymentSummaryTitle = jData['paymentSummaryTitle'];
+        }
+
+        if (jData.containsKey('paymentSummaryAmountColor')){
+          paymentSummaryAmountColor = HexColor(jData['paymentSummaryAmountColor']);
+        }
+
+        try {
+          setState(() {
+            _widget = PayFast(
+              useSandBox: useSandBox, 
+              passPhrase: passPhrase, 
+              data: paymentData, 
+              onsiteActivationScriptUrl: onsiteActivationScriptUrl, 
+              onPaymentCancelled: () => onPaymentCancelled(), 
+              onPaymentCompleted: () => onPaymentCompleted(),
+              payButtonText: payButtonText,
+              onPaymentCancelledText: onPaymentCancelledText,
+              onPaymentCompletedText: onPaymentCompletedText,
+              paymentSummaryTitle: paymentSummaryTitle,
+              paymentSummaryAmountColor: paymentSummaryAmountColor
+            );
+          });
+        } catch (e, stackTrace) {
+          debugPrint('Error during setState: $e');
+          debugPrint('Stack trace: $stackTrace');
+        }
+      }
+    } on PlatformException catch (error) {
+      debugPrint('PlatformException: $error');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: (showAppBar) ? AppBar(
+        backgroundColor: (appBarBackgroundColor.isNotEmpty) ? HexColor(appBarBackgroundColor) : Colors.blue,
+        title: (appBarTitle.isNotEmpty) ? Text(appBarTitle) : Text(widget.title),
+      ) : null,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: (_widget == null) 
+        ? Container(
+            width: 24,
+            height: 24,
+            padding: const EdgeInsets.all(2.0),
+            child: CircularProgressIndicator(
+              color: spinnerColor,
+              strokeWidth: 3,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          )
+        : _widget,
+    ));
   }
 }
